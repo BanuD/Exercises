ba9edf94eb302240693c323fc49c807550250686
banud <banudalamanli@gmail.com>
2015-05-30 11:15:50 -0700

    Fixed bugs - now it's working

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -11,25 +11,31 @@ class Player
     end
 
     def get_user_input
-        @input = gets.chomp
+        user_input = gets.chomp # Format is going to be "0,0"
+        x = user_input[0].to_i
+        y = user_input[2].to_i
+        @input = [x, y]
     end
+
 
 end
 
 class Board
-    attr_accessor :board
+    attr_accessor :layout
 
     def initialize
-        @board = Array.new(3, Array.new(3, "-"))
+        # @layout = Array.new(3, Array.new(3, "-"))
+        # p @layout.map(&:object_id)
+        @layout = Array.new(3) { Array.new(3, "-") }
     end
 
     def render_board
-        puts " #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]}\n #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]}\n #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]}\n"
+        puts " #{@layout[0][0]} | #{@layout[0][1]} | #{@layout[0][2]}\n #{@layout[1][0]} | #{@layout[1][1]} | #{@layout[1][2]}\n #{@layout[2][0]} | #{@layout[2][1]} | #{@layout[2][2]}\n"
     end
 
     def full?
-        current_board = @board.flatten
-        (current_board.include? "-") ? false : true
+        current_layout = @layout.flatten
+        (current_layout.include? "-") ? false : true
     end
 end
 
@@ -38,6 +44,7 @@ class Game
     attr_accessor :board, :status
     def initialize
         @board = Board.new
+        @layout = @board.layout
         @status = "on"
         @player1 = Player.new("O")
         @player2 = Player.new("X")
@@ -45,11 +52,11 @@ class Game
 
     def check_rows(player)
         sign = player.sign
-        board_rows = @board.dup
+        board_rows = @layout.dup
         i = 0
-        while i < @board.length
+        while i < @layout.length
             if board_rows[i] == [sign, sign, sign]
-                @player.status = "winner"
+                player.status = "winner"
                 @status = "end"
             end
             i += 1
@@ -58,11 +65,11 @@ class Game
 
     def check_columns(player)
         sign = player.sign
-        board_columns = @board.dup.transpose
+        board_columns = @layout.dup.transpose
         i = 0
-        while i < @board.length
-            if board_colums[i] == [sign, sign, sign]
-                @player.status = "winner"
+        while i < board_columns.length
+            if board_columns[i] == [sign, sign, sign]
+                player.status = "winner"
                 @status = "end"
             end
             i += 1
@@ -71,33 +78,47 @@ class Game
 
     def check_diagonals(player)
         sign = player.sign
-        board_diagonals = @board.dup
-        if (board_diagonals[0][0] == sign && board_diagonals[1][1] == sign && board_diagonals[2][2] == sign) || (board_diagonals[0][2] == sign && board_diagonals[1][1] == sign && board_diagonals[2][0] == sign)
-            @player.status = "winner"
+        diagonal1 = [@layout[0][0], @layout[1][1], @layout[2][2]]
+        diagonal2 = [@layout[0][2], @layout[1][1], @layout[2][0]]
+
+        if (diagonal1 == [sign, sign, sign]) || (diagonal2 == [sign, sign, sign])
+            player.status = "winner"
             @status = "end"
         end
+
+        # board_diagonals = @layout.dup
+        # if (board_diagonals[0][0] == sign && board_diagonals[1][1] == sign && board_diagonals[2][2] == sign) || (board_diagonals[0][2] == sign && board_diagonals[1][1] == sign && board_diagonals[2][0] == sign)
+        #     player.status = "winner"
+        #     @status = "end"
+        # end
     end
 
 
     def insert_sign(player)
-        puts "Please put in the coordinates!"
+        puts "Please put in your coordinates!"
         input = player.get_user_input
-        x = input[1].to_i
-        puts "x: #{x}"
-        y = input[3]
-        puts "y: #{y}"
-        puts "board[x] #{@board[x]}"
-        raise 'You may only choose an empty slot' unless @board[x][y] == "-"
+        x = input[0]
+        y = input[1]
+        raise 'You may only choose an empty slot' unless @layout[x][y] == "-"
         raise 'You have to be within board limits' if (x > 2 || y > 2)
-        @board[x][y] = player.sign
+        # puts "x: #{x}   y: #{y}"
+        # puts "@layout[x][y] before => #{@layout[x][y]}"
+        @layout[x][y] = player.sign
+        # puts "@layout[x][y] after => #{@layout[x][y]}"
+        # puts "layout: #{@layout}"
     end
 
-    def play(player)
+    def play(player, game)
+        puts "These are the coordinates:"
+        puts "0,0 | 0,1 | 0,2\n1,0 | 1,1 | 1,2\n2,0 | 2,1 | 2,2"
+        puts "Here is the board"
+        game.board.render_board
         insert_sign(player)
-        check_rows(player.sign)
-        check_columns(player.sign)
-        check_diagonals(player.sign)
-        return "#{player} is the winner!!" if player.status == "winner"
+        check_rows(player)
+        check_columns(player)
+        check_diagonals(player)
+        puts "This is the current board"
+        game.board.render_board
     end
 end
 
@@ -105,26 +126,29 @@ end
 # play_game() should play a full two human player game
 def play_tic_tac_toe
     game = Game.new
-    puts "Here is the board"
-    game.board.render_board
     player1 = game.player1
     player2 = game.player2
 
     while game.status == "on"
-        game.play(player1)
-        game.play(player2)
+        puts "\n\n"
+        puts "PLAYER 1 playing"
+        game.play(player1, game)
+        return puts "You are the winner!!" if player1.status == "winner"
+
+        puts "\n\n"
+        puts "PLAYER 2 playing"
+        game.play(player2, game)
+        return puts "You are the winner!!" if player2.status == "winner"
 
         if game.board.full?
             game.status == "end"
             puts "It's a tie!! try again"
         end
-        board.render_board
     end
+    game.board.render_board
 end
 
 play_tic_tac_toe
-
-
 
 
 
